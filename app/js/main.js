cfdc6d6217c9738c71e60e95cdae81b3215012fd
viktor <radchenkovictor@yahoo.com>
2020-11-27 14:37:32 +0400

    added video section

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
-$(function() {
+$(function () {
   $(".top-slider__slider").slick({
-    arrows: false,
     dots: true,
+    arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 10000,
+    autoplaySpead: 10000,
+  });
+
+  $(".featured__item").hover(function () {
+    $(this).find(".group-icons").toggleClass("group-icons--hover");
+  });
+
+  const featured = mixitup(".featured__items", {
+    selectors: {
+      control: ".featured__btn",
+    },
+  });
+
+  const newProducts = mixitup(".new-design__content", {
+    selectors: {
+      control: ".new-design__filter-btn",
+    },
   });
 });
